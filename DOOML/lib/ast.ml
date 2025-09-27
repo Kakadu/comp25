293e8567697ace49819037615ea7d9e7cc876527
Georgiy Belyanin <belyaningeorge@ya.ru>
2025-09-27 16:50:31 +0300

    parser: introduce basic AST and parser
    This is quasi-initial commit of the DOOML language compiler done in
    terms of functional language compilers 2025 SPbU course.
    
    Basically, it is OCaml-like language with (drastically) truncated list
    of features. This patch introduces a basic AST and parser with
    pretty-printing facilities. Only integers, units and tuples are
    supported yet.
    
    This will be implemented by Georgiy Belyanin and Ignatiy Sergeev.

---
+++ DOOML/lib/ast.ml
@@ -0,0 +1,109 @@
+type ident = string
+
+let pp_ident ppf ident =
+  match String.get ident 0 with
+  | 'a' .. 'z' | 'A' .. 'Z' | '_' -> Format.fprintf ppf "%s" ident
+  | _ -> Format.fprintf ppf "(%s)" ident
+;;
+
+let pp_sep_space ppf () = Format.fprintf ppf " "
+let pp_sep_quote ppf () = Format.fprintf ppf ", "
+
+type ty =
+  | Int
+  | Bool
+  | List of ty
+  | Unit
+[@@deriving show, variants]
+
+type pattern =
+  | PUnit (** () *)
+  | Plug (** _ *)
+  | Ident of ident
+  | Tuple of pattern list
+[@@deriving variants]
+
+let rec pp_pattern ppf = function
+  | PUnit -> Format.fprintf ppf "()"
+  | Plug -> Format.fprintf ppf "_"
+  | Ident s -> Format.fprintf ppf "%s" s
+  | Tuple ss ->
+    Format.fprintf ppf "%a" (Format.pp_print_list ~pp_sep:pp_sep_quote pp_pattern) ss
+;;
+
+type decl_body = pattern [@@deriving show]
+
+type rec_flag =
+  | Rec
+  | NonRec
+[@@deriving variants]
+
+type expr =
+  | Const of int
+  | Var of ident
+  | App of expr * expr
+  | Let of rec_flag * pattern * expr * expr
+  | Ite of expr * expr * expr
+  | Fun of pattern list * expr
+[@@deriving variants]
+
+let rec pp_expr ppf = function
+  | Const c -> Format.fprintf ppf "%d" c
+  | Var ident -> Format.fprintf ppf "%a" pp_ident ident
+  | App (f, (Const _ as arg)) | App (f, (Var _ as arg)) ->
+    Format.fprintf ppf "%a %a" pp_expr f pp_expr arg
+  | App (f, arg) -> Format.fprintf ppf "%a (%a)" pp_expr f pp_expr arg
+  | Let (rec_flag, pattern, bind, body) ->
+    let rec_flag =
+      match rec_flag with
+      | Rec -> "rec "
+      | NonRec -> ""
+    in
+    Format.fprintf
+      ppf
+      "let %s%a = %a in@ %a"
+      rec_flag
+      pp_pattern
+      pattern
+      pp_expr
+      bind
+      pp_expr
+      body
+  | Fun (patterns, body) ->
+    Format.fprintf
+      ppf
+      "@[<v 2>fun %a ->@ @[%a@]@]"
+      (Format.pp_print_list ~pp_sep:pp_sep_space pp_pattern)
+      patterns
+      pp_expr
+      body
+  | Ite (cond, then_, else_) ->
+    Format.fprintf
+      ppf
+      "if @[%a@] then@ @[%a@]@ else@ @[%a@]"
+      pp_expr
+      cond
+      pp_expr
+      then_
+      pp_expr
+      else_
+;;
+
+type top_level = LetDecl of rec_flag * pattern * expr [@@deriving variants]
+
+let pp_top_level ppf = function
+  | LetDecl (rec_flag, pattern, body) ->
+    let rec_flag =
+      match rec_flag with
+      | Rec -> "rec "
+      | NonRec -> ""
+    in
+    Format.fprintf
+      ppf
+      "@[<v 2>let %s%a =@ @[%a@]@]@.;;@."
+      rec_flag
+      pp_pattern
+      pattern
+      pp_expr
+      body
+;;
