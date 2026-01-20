db035a56b648149888e8310e737f76a10207fac9
ignat.sergeev <ignat.sergeev@softcom.su>
2026-01-21 00:53:43 +0300

    boxing

--- DOOML/lib/runtime/runtime.c
+++ DOOML/lib/runtime/runtime.c
@@ -63,6 +63,8 @@ typedef struct {
 static const uint64_t GC_BANK_SIZE = MEM;
 static GC gc;
 
+#define debugf printf
+
 void init_gc() {
     GCStats init_stats = {
         .runs = 0,
@@ -77,14 +79,15 @@ void init_gc() {
         .to_bank_size = GC_BANK_SIZE,
         .to_bank_start = malloc(GC_BANK_SIZE),
     };
-    gc.free_space_start = gc.from_bank_start;
+    init_gc.free_space_start = init_gc.from_bank_start;
     gc = init_gc;
 }
 
 int64_t *alloc_gc(uint32_t size, GCObjTag tag) {
     int64_t *ptr = gc.free_space_start;
-    uint32_t taken_bytes = (ptr - gc.from_bank_start) / 8;
+    uint32_t taken_bytes = ((uint32_t) (ptr - gc.from_bank_start)) / 8;
     uint32_t free_space = gc.from_bank_size - taken_bytes;
+    debugf("> alloc_gc(%u): had %u free space\n", size, free_space);
 
     if (free_space < size) {
         fprintf(stderr, "GC OOM\n");
@@ -104,28 +107,36 @@ int64_t box_imm(int64_t n) {
     return (n << 1) + 1;
 }
 
-int64_t unbox_imm(int64_t n) {
-    return n >> 1;
-}
-
 bool is_imm(int64_t n) {
     return n | 1;
 }
 
+int64_t unbox(int64_t n) {
+    if (!is_imm(n)) {
+        return n;
+    }
+    return n >> 1;
+}
+
 int64_t create_tuple(int64_t size, int64_t init) {
-  GCTuple *tuple = (GCTuple *)alloc_gc(sizeof(GCTuple) + size, Tuple);
+  GCTuple *tuple = (GCTuple *)alloc_gc(sizeof(GCTuple) + size * 8, Tuple);
   tuple->size = size;
-  for (int64_t i = 0; i < size; i++)
+  for (int64_t i = 0; i < size; i++) {
     tuple->fields[i] = ((int64_t*) init)[i];
+  }
 
   return (int64_t) tuple;
 }
 
 int64_t tuple_nth(int64_t tuple, int64_t i) {
-  return ((GCTuple*) tuple)->fields[i];
+  int64_t unboxed_i = unbox(i);
+  GCTuple *tuple_ptr = (GCTuple*) tuple;
+  if (unboxed_i >= tuple_ptr->size) {
+    fprintf(stderr, "tuple_nth: index is out of bounds\n");
+    exit(1);
+  }
+  return tuple_ptr->fields[unboxed_i];
 }
-
-#define debugf printf
 
 int64_t create_closure(int64_t callee, int64_t arity, int64_t argc, int64_t argv_) {
   assert(argc < arity);
@@ -134,7 +145,7 @@ int64_t create_closure(int64_t callee, int64_t arity, int64_t argc, int64_t argv
 
   int64_t *argv = (int64_t*) argv_;
 
-  GCClosure *closure = (GCClosure *)alloc_gc(sizeof(GCClosure) + arity, Closure);
+  GCClosure *closure = (GCClosure *)alloc_gc(sizeof(GCClosure) + arity * 8, Closure);
   closure->callee = callee;
   closure->arity = arity;
   closure->argc = argc;
@@ -150,7 +161,7 @@ int64_t create_closure(int64_t callee, int64_t arity, int64_t argc, int64_t argv
 GCClosure *copy_closure(GCClosure *closure) {
   debugf("> copy_closure(%ld)\n", (int64_t) closure);
 
-  GCClosure *closure2 = (GCClosure *)alloc_gc(sizeof(GCClosure) + closure->arity, Closure);
+  GCClosure *closure2 = (GCClosure *)alloc_gc(sizeof(GCClosure) + closure->arity * 8, Closure);
   closure2->callee = closure->callee;
   closure2->arity = closure->arity;
   closure2->argc = closure->argc;
@@ -167,7 +178,7 @@ int64_t closure_apply(int64_t closure_, int64_t argc, int64_t argv_) {
   debugf("> closure_apply(%ld, %ld, %ld)\n", closure_, argc, argv_);
 
   int64_t *argv = (int64_t*) argv_;
-  GCClosure *closure = copy_closure ((GCClosure *)closure_);
+  GCClosure *closure = copy_closure((GCClosure *)closure_);
   debugf("  closure_apply: closure stats %ld %ld %ld\n", closure->callee, closure->arity, closure->argc);
   int64_t current_argc = closure->argc;
   for (int64_t i = 0; i < argc; i++) {
@@ -186,7 +197,7 @@ int64_t closure_apply(int64_t closure_, int64_t argc, int64_t argv_) {
 }
 
 int64_t print_int(int64_t n) {
-  return printf("%ld\n", n);
+  return box_imm(printf("%ld\n", unbox(n)));
 }
 
 void exit2(void) {
