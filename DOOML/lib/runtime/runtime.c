47ceb0753af4bb3fa683d7c9e421cb490856ad27
ignat.sergeev <ignat.sergeev@softcom.su>
2026-01-23 04:00:23 +0300

    finalize gc runtime

--- DOOML/lib/runtime/runtime.c
+++ DOOML/lib/runtime/runtime.c
@@ -64,7 +64,28 @@ static const uint64_t GC_BANK_SIZE = MEM;
 static GC gc;
 static int64_t *initial_sp; 
 
+#ifdef DEBUG
 #define debugf printf
+#define debug_call(...) __VA_ARGS__
+#else
+#define debugf(...)
+#define debug_call(...)
+#endif
+
+int64_t box_imm(int64_t n) {
+    return (n << 1) + 1;
+}
+
+bool is_imm(int64_t n) {
+    return n & 1;
+}
+
+int64_t unbox(int64_t n) {
+    if (!is_imm(n)) {
+        return n;
+    }
+    return n >> 1;
+}
 
 void gc_init() {
     GCStats init_stats = {
@@ -81,11 +102,61 @@ void gc_init() {
         .to_bank_start = malloc(GC_BANK_SIZE),
     };
     init_gc.free_space_start = init_gc.from_bank_start;
+    debugf("> bank 0 ranges: %ld : %ld\n", (int64_t)init_gc.from_bank_start, (int64_t)(init_gc.from_bank_start + init_gc.from_bank_size));
+    debugf("> bank 1 ranges: %ld : %ld\n", (int64_t)init_gc.to_bank_start, (int64_t)(init_gc.to_bank_start + init_gc.to_bank_size));
     gc = init_gc;
 }
 
-void sp_init(int64_t sp) {
-    initial_sp = (int64_t *)sp;
+static inline int64_t *get_sp() {
+    return (int64_t *)__builtin_frame_address(0);
+}
+
+void sp_init() {
+    initial_sp = get_sp();
+}
+
+void print_obj_helper(int64_t ptr) {
+    if (is_imm(ptr)) {
+        printf("int %ld", ptr);
+        return;
+    }
+    GCObjTag tag = ((GCObjHeader *)ptr)->tag;
+    if (tag == Forward) {
+        printf("Forward -> %ld: ", ((GCForward *)ptr)->ptr);
+        print_obj_helper(((GCForward *)ptr)->ptr);
+    }
+    if (tag == Closure) {
+        GCClosure *closure = (GCClosure *)ptr;
+        printf("Closure %ld(", closure->callee);
+        for (int i = 0; i < closure->arity; i++) {
+            if (i < closure->argc) {
+                print_obj_helper(closure->args[i]);
+            } else {
+                printf("...");
+            }
+            if (i != closure->arity - 1) {
+                printf(", ");
+            }
+        }
+        printf(")");
+    }
+    if (tag == Tuple) {
+        GCTuple *tuple = (GCTuple *)ptr;
+        printf("Tuple (");
+        for (int i = 0; i < tuple->size; i++) {
+            print_obj_helper(tuple->fields[i]);
+            if (i != tuple->size - 1) {
+                printf(", ");
+            }
+        }
+        printf(")");
+    }
+}
+
+void debug_print_value(int64_t ptr) {
+    debug_call(printf("%ld: ", ptr));
+    debug_call(print_obj_helper(ptr));
+    debug_call(printf("\n"));
 }
 
 void gc_collect();
@@ -94,7 +165,6 @@ int64_t *gc_alloc(uint32_t size, GCObjTag tag) {
     int64_t *ptr = gc.free_space_start;
     uint32_t taken_bytes = ((uint32_t) (ptr - gc.from_bank_start)) / 8;
     uint32_t free_space = gc.from_bank_size - taken_bytes;
-    debugf("> alloc_gc(%u): had %u free space\n", size, free_space);
 
     if (free_space < size) {
         gc_collect();
@@ -117,56 +187,26 @@ int64_t *gc_alloc(uint32_t size, GCObjTag tag) {
 }
 
 GCClosure *gc_alloc_closure_base(int64_t callee, int64_t arity, int64_t argc) {
-  GCClosure *closure = (GCClosure *)gc_alloc(sizeof(GCClosure) + arity * 8, Closure);
+  int64_t size = sizeof(GCClosure) + arity * 8;
+  GCClosure *closure = (GCClosure *)gc_alloc(size, Closure);
   closure->callee = callee;
   closure->arity = arity;
   closure->argc = argc;
+  debugf("< alloc(%ld): closure %ld with %ld(%ld out of %ld)\n", size, (int64_t)closure, callee, argc, arity);
   return closure;
 }
 
 GCTuple *gc_alloc_tuple_base(int64_t size) {
-  GCTuple *tuple = (GCTuple *)gc_alloc(sizeof(GCTuple) + size * 8, Tuple);
+  int64_t obj_size = sizeof(GCClosure) + size * 8;
+  GCTuple *tuple = (GCTuple *)gc_alloc(obj_size, Tuple);
   tuple->size = size;
+  debugf("< alloc(%ld): tuple %ld with size %ld\n", obj_size, (int64_t)tuple, size);
   return tuple;
 }
 
 void gc_make_fwd(int64_t ptr, int64_t new_ptr) {
     ((GCObjHeader *)ptr)->tag = Forward;
     ((GCForward *)ptr)->ptr = new_ptr;
-}
-
-int64_t box_imm(int64_t n) {
-    return (n << 1) + 1;
-}
-
-bool is_imm(int64_t n) {
-    return n | 1;
-}
-
-int64_t unbox(int64_t n) {
-    if (!is_imm(n)) {
-        return n;
-    }
-    return n >> 1;
-}
-
-int64_t *get_sp() {
-    return (int64_t *)__builtin_frame_address(0);
-}
-
-int64_t gc_scan_ptrs_on_stack(int64_t *range_start, int64_t *range_end, int64_t** ptrs) {
-    int64_t *sp = get_sp(); 
-    int64_t ptrs_idx = 0;
-    // start from stack bottom to preserve order for minimizing forward ptr usages
-    for (int64_t i = 0; initial_sp + i < sp; i++) {
-        int64_t stack_val = initial_sp[i];
-        if (!is_imm(stack_val) && stack_val > (int64_t)range_start && stack_val < (int64_t)range_end) {
-            ptrs[ptrs_idx] = initial_sp + i;
-            ptrs_idx += 1;
-        }
-    }
-
-    return ptrs_idx;
 }
 
 int64_t gc_mark_and_copy(int64_t ptr) {
@@ -216,8 +256,9 @@ int64_t gc_mark_and_copy(int64_t ptr) {
 }
 
 void gc_collect() {
-    int64_t *ptrs[UINT32_MAX];
-    int64_t ptrs_size = gc_scan_ptrs_on_stack(gc.from_bank_start, gc.free_space_start, ptrs);
+    int64_t *sp = get_sp(); 
+    int64_t gc_bank_range_start = (int64_t)gc.from_bank_start;
+    int64_t gc_bank_range_end = (int64_t)gc.free_space_start;
 
     int64_t *to_bank_start = gc.to_bank_start;
     int64_t to_bank_size = gc.to_bank_size;
@@ -229,12 +270,19 @@ void gc_collect() {
     gc.stats.bank_idx = 1 - gc.stats.bank_idx;
     gc.stats.size = 0;
 
-    for (int64_t i = 0; i < ptrs_size; i++) {
-        int64_t *old_ptr_on_stack = ptrs[i];
-        *old_ptr_on_stack = gc_mark_and_copy(*old_ptr_on_stack);
+    debugf("> gc_collect\n");
+    for (int64_t stack_cell = (int64_t)initial_sp; stack_cell >= (int64_t)sp; stack_cell--) {
+        int64_t obj_ptr = *(int64_t*)stack_cell;
+        if (!is_imm(obj_ptr) && obj_ptr >= gc_bank_range_start && obj_ptr < gc_bank_range_end) {
+            debugf("  gc_root: %ld\n", obj_ptr);
+            debug_print_value(obj_ptr);
+            *(int64_t*)stack_cell = gc_mark_and_copy(obj_ptr);
+            debugf("  new pointer on stack: %ld\n", *(int64_t*)stack_cell);
+        }
     }
 
     gc.stats.runs += 1;
+    debugf("< gc_collect\n");
 }
 
 void collect(int64_t unit) {
@@ -263,50 +311,46 @@ int64_t tuple_nth(int64_t tuple, int64_t i) {
 int64_t create_closure(int64_t callee, int64_t arity, int64_t argc, int64_t argv_) {
   assert(argc < arity);
 
-  debugf("> create_closure(%ld, %ld, %ld, %ld)\n", callee, arity, argc, argv_);
-
   GCClosure *closure = gc_alloc_closure_base(callee, arity, argc);
 
   int64_t *argv = (int64_t*) argv_;
   for (int64_t i = 0; i < argc; i++) {
     closure->args[i] = argv[i];
   }
-  debugf("< create_closure() -> %ld\n", (int64_t) closure);
 
   return (int64_t) closure;
 }
 
 GCClosure *copy_closure(GCClosure *closure) {
-  debugf("> copy_closure(%ld)\n", (int64_t) closure);
-
   GCClosure *closure2 = gc_alloc_closure_base(closure->callee, closure->arity, closure->argc);
   for (int64_t i = 0; i < closure->argc; i++) {
     closure2->args[i] = closure->args[i];
   }
 
-  debugf("< copy_closure() -> %ld\n", (int64_t) closure2);
-
   return closure2;
 }
 
 int64_t closure_apply(int64_t closure_, int64_t argc, int64_t argv_) {
-  debugf("> closure_apply(%ld, %ld, %ld)\n", closure_, argc, argv_);
+  debugf("> closure_apply\n");
+  debugf("  orig: ");
+  debug_print_value(closure_);
 
   int64_t *argv = (int64_t*) argv_;
   GCClosure *closure = copy_closure((GCClosure *)closure_);
-  debugf("  closure_apply: closure stats %ld %ld %ld\n", closure->callee, closure->arity, closure->argc);
   int64_t current_argc = closure->argc;
   for (int64_t i = 0; i < argc; i++) {
-    debugf("  closure_apply: arg %ld %ld is %ld in the orig\n", i, argv[i], i + current_argc);
     closure->args[i + current_argc] = argv[i];
     closure->argc++;
   }
 
+  debugf("  applied: ");
+  debug_print_value((int64_t)closure);
+
   if (closure->argc >= closure->arity) {
-    debugf("  closure_apply: calling %ld %ld %ld\n", closure->callee, closure->arity, (int64_t) closure->args);
+    debugf("  closure_apply: calling\n");
     return call_function((void*) closure->callee, closure->arity, closure->args);
   } else {
-    debugf("  closure_apply: returning a new closure %ld %ld %ld\n", closure->callee, closure->arity, (int64_t) closure->args);
+    debugf("  closure_apply: returning a new closure");
     return (int64_t) closure;
   }
 }
